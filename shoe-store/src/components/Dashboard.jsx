1862ef259b76ad11003e7b7a5c3bdc15dfa3a8ee
Tom Darby <tomd.57@live.com>
2021-11-30 14:02:07 +0100

    socket connected

--- shoe-store/src/components/Dashboard.jsx
+++ shoe-store/src/components/Dashboard.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Dashboard = () => {
+
+    const [order, setOrder] = useState([]);
     
-    var ws = new WebSocket('ws://localhost:8080/');
-    ws.onmessage = function(event) {
-    console.log(event.data);
-    };
+    const socket = () => {
+        var ws = new WebSocket('ws://localhost:8080/');
+        ws.onmessage = function(event) {
+        console.log(event.data);
+        setOrder( order => [...order, event.data])
+        };
+    }
+
+    useEffect(() => {
+        socket()
+    },[])
 
     return(
-        <h1>dash</h1>
+        <div>
+            <p>hey</p>
+            <button onClick={ () => console.log(order)}>click me</button>
+        </div>
     )
 }
 
