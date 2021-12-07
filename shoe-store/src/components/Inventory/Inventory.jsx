a9108d9e03177ea08797044741007603e08b409b
Tom Darby <tomd.57@live.com>
2021-12-07 14:58:03 +0100

    breakthrough moment

--- shoe-store/src/components/Inventory/Inventory.jsx
+++ shoe-store/src/components/Inventory/Inventory.jsx
@@ -11,11 +11,19 @@ const Inventory = ({newOrders}) => {
     // https://stackoverflow.com/questions/48568571/increment-value-of-one-element-of-an-json-array-of-objects
 
     const [stores, setStores] = useState([]);
+    const [obj, setObj] = useState({});
 
     useEffect(() => {
         //console.log(newOrders);
-        setStores( (newOrders.map(item => item.store)))
+        setStores((newOrders.map(item => item.store)))
         //console.log(stores);
+        //setObj((newOrders.map(order => order.store)).reduce((obj, data) => ( obj[data] = {} ,obj),{}) );
+        setObj(newOrders.reduce(function (x,y) {
+            x[y.store] = x[y.store] || [];
+            x[y.store].push(y)
+            return x
+        }, Object.create(null)));
+        console.log(obj);
     },[newOrders]);
 
     return(
